e3fb14969cdf97e6b292af51a0f2387f098f825f
Erno Wever <ernowever@Ernos-MacBook-Pro.local>
2017-12-04 20:53:32 +0100

    Change select defaultValue to value

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,7 +5,7 @@ const Book = (props) => (
     <div className="book-top">
       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.smallThumbnail})`}}></div>
       <div className="book-shelf-changer">
-        <select defaultValue={props.book.shelf}>
+        <select value={props.book.shelf}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
