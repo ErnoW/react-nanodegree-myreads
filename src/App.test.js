d58f1e559644758f267d96dfe41fa0e4c2b6efb5
Erno Wever <34257502+ErnoW@users.noreply.github.com>
2017-12-09 01:38:44 +0100

    Setup test enviroment

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { mount } from 'enzyme'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
- is not required.
-**/
-
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
-
-
+  mount(<BrowserRouter><App /></BrowserRouter>);
+});
